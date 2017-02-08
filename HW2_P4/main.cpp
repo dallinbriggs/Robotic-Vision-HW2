de72137235139185df67f44700e6f85b26d2c8e5
Dallin Briggs <dallinbriggs@gmail.com>
2017-02-08 14:34:54 -0700

    Task 4 working

---
+++ HW2_P4/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/videoio.hpp"
+#include <fstream>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    Mat image;
+    Mat image_undistort;
+    Mat image_diff;
+    vector<Point2f> corners;
+    Size patternsize;
+    string filename;
+    string header;
+    string tail;
+    Size imageSize;
+    Mat distCoeffs;
+    Mat rvec;
+    Mat tvec;
+    Mat cameraMatrix;
+    FileStorage fs("/home/dallin/robotic_vision/HW2/HW2_P4/Intrinsic_calibration.xml", FileStorage::READ);
+    fs["CameraMatrix"] >> cameraMatrix;
+    fs["DistortionCoefficients"] >> distCoeffs;
+
+    Mat ImagePoints;
+    Mat ObjectPoints;
+    FileStorage fs2("/home/dallin/robotic_vision/HW2/HW2_P4/DataPoints.xml", FileStorage::READ);
+    fs2["ImagePoints"] >> ImagePoints;
+    fs2["ObjectPoints"] >> ObjectPoints;
+
+    bool PNP_solved = solvePnP(ObjectPoints,ImagePoints,cameraMatrix,distCoeffs,rvec, tvec,false,SOLVEPNP_ITERATIVE);
+    Rodrigues(rvec,rvec,noArray());
+    if(PNP_solved)
+    {
+        cout << rvec << endl << tvec << endl;
+    }
+    else
+    {
+        cout << "Error in solving PNP" << endl;
+        return -1;
+    }
+    image = imread("/home/dallin/robotic_vision/HW2/HW2_P4/Object_with_Corners.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+    undistort(image,image_undistort,cameraMatrix,distCoeffs,noArray());
+    absdiff(image_undistort,image,image_diff);
+    imshow("Briggs",image);
+
+
+
+    waitKey(0);
+
+    return 0;
+}
